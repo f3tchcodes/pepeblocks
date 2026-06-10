c89c183f020de5d12f9f09213318f182e71e62e5
f3tch <f3tch.169@gmail.com>
2026-06-10 23:06:20 +0500

    small changes

--- src/modules/blocks.ts
+++ src/modules/blocks.ts
@@ -10,7 +10,7 @@ export class PepeBlocks {
 
     /**
      * Returns the count of all blocks on the blockchain.
-     * @returns Number of blocks in the blockchain
+     * @returns Number of blocks in the blockchain.
      */
     public async count(): Promise<number> {
         const res = await this.client._getReq("/api/getblockcount");
@@ -29,8 +29,8 @@ export class PepeBlocks {
 
     /**
      * Takes in index of a block and returns it's hash.
-     * @param index The block index
-     * @returns hash value of the block
+     * @param index The block index.
+     * @returns hash value of the block.
      */
     public async getHash(index: number): Promise<string> {
         const res = await this.client._getReq(`/api/getblockhash?index=${index}`);
@@ -40,8 +40,8 @@ export class PepeBlocks {
 
     /**
      * Takes in hash of the block and returns it's information.
-     * @param hash The block has
-     * @returns Information about the block
+     * @param hash The block hash.
+     * @returns Information about the block.
      */
     public async getBlock(hash: string): Promise<BlockResponse> {
         const res = await this.client._getReq(`/api/getblock?hash=${hash}`);
