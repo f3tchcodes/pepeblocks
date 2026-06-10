b917989d5bf7d1ac06dd70bcb04aaaf2641b5fa1
f3tch <f3tch.169@gmail.com>
2026-06-10 14:59:16 +0500

    comments

--- src/client/pepeClient.ts
+++ src/client/pepeClient.ts
@@ -4,6 +4,8 @@ export class PepeClient {
     public baseUrl: string;
     
     constructor(options: ClientOptions = {}) {
+        // if baseUrl is given when initiating the object, use that
+        // otherwise use the default website
         const defaultUrl = "https://pepeblocks.com/";
         const chosenUrl = options.baseUrl || defaultUrl;
         this.baseUrl = chosenUrl.endsWith('/') ? chosenUrl.slice(0, -1) : chosenUrl;
--- src/interfaces/clientOptions.ts
+++ src/interfaces/clientOptions.ts
@@ -1,3 +1,4 @@
+// interface for the client options inside PepeClient
 export interface ClientOptions {
     baseUrl?: string;
 }
