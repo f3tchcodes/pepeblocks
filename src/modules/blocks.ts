2e3b92debd6d59c5a5d2e7a7c0b893f6c20c6179
f3tch <f3tch.169@gmail.com>
2026-06-10 16:52:46 +0500

    parsing text and handling errors while receiving total blocks count

--- src/modules/blocks.ts
+++ src/modules/blocks.ts
@@ -9,9 +9,21 @@ export class PepeBlocks {
 
     public async count(): Promise<number> {
         const res = await fetch(`${this.client.baseUrl}/api/getblockcount`);
+
+        // if response status is not 200, throw error
         if (!res.ok) {
             throw new Error(`Request failed with the status code: ${res.status}`);
         }
-        return res.json();
+        
+        // convert text into number
+        const text = await res.text();
+        const count = Number(text);
+
+        // if it's not a number, throw error
+        if (Number.isNaN(count)) {
+            throw new Error(`Invalid block count received: ${text}`);
+        }
+
+        return count;
     }
 }
