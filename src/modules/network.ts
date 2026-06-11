13bf798d7ae9988960737d9218a6bb9080bb55fe
f3tch <f3tch.169@gmail.com>
2026-06-11 10:00:23 +0500

    network difficulty

--- src/modules/network.ts
+++ src/modules/network.ts
@@ -6,4 +6,39 @@ export class PepeNetwork {
     constructor(client: PepeClient) {
         this.client = client;
     }
+
+    /**
+     * Fetches the current network difficulty.
+     * @returns The difficulty value as a number.
+     */
+    public async difficult(): Promise<number> {
+        const res = await this.client._getReq(`/api/getdifficulty`);
+
+        // convert text into number
+        const text = await res.text();
+        const difficulty = Number(text);
+
+        // if it's not a number, throw error
+        if (text === "" || Number.isNaN(difficulty)) {
+            throw new Error(`Invalid difficulty received: ${text}`);
+        }
+
+        return difficulty;
+    }
+
+    public async hashrate(): Promise<number> {
+        const res = await this.client._getReq(`/api/getnetworkhashps`);
+
+        // convert text into number
+        const text = await res.text();
+        const hashrate = Number(text);
+
+        // if it's not a number, throw error
+        if (text === "" || Number.isNaN(hashrate)) {
+            throw new Error(`Invalid hash rate received: ${text}`);
+        }
+
+        return hashrate;
+
+    }
 }
