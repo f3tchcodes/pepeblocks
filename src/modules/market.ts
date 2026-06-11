801a2137cdefb1fcda2bdc718085eda5a079f4f2
f3tch <f3tch.169@gmail.com>
2026-06-11 11:56:18 +0500

    current price

--- src/modules/market.ts
+++ src/modules/market.ts
@@ -8,10 +8,18 @@ export class PepeMarket {
     }
 
     /**
-     * Fetches the current money supply from the API.
-     * @returns Money supply number.
+     * Fetches the current money supply of pepecoin from the API.
+     * @returns Current money supply number.
      */
     public async moneysupply(): Promise<number> {
         return await this.client._getReqNumber(`/ext/getmoneysupply`);
+    }
+
+    /**
+     * Fetches the current price of pepecoin from the API.
+     * @returns Current price number.
+     */
+    public async price(): Promise<number> {
+        return await this.client._getReqNumber(`/ext/getcurrentprice`);
     }
 }
