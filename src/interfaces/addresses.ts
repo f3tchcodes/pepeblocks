379f164f7004f882cb1d9bea9e7a6acc6ac0a2df
f3tch <f3tch.169@gmail.com>
2026-06-11 10:53:46 +0500

    get information about the address

---
+++ src/interfaces/addresses.ts
@@ -0,0 +1,13 @@
+export interface AddressProfileResponse {
+    page: number;
+    totalPages: number;
+    itemsOnPage: number;
+    address: string;
+    balance: string;
+    totalReceived: string;
+    totalSent: string;
+    unconfirmedBalance: string;
+    unconfirmedTxs: number;
+    txs: number;
+    txids: string[];
+}
--- src/modules/addresses.ts
+++ src/modules/addresses.ts
@@ -1,9 +1,20 @@
 import { type PepeClient } from "#client/pepeClient";
+import { type AddressProfileResponse } from "#interfaces/addresses";
 
 export class PepeAddresses {
     private client: PepeClient;
 
     constructor(client: PepeClient) {
         this.client = client;
+    }
+
+    /**
+     * Fetches information about a wallet such as balance, TXIDs, total received etc.
+     * @param address The address of a wallet.
+     * @returns Object containing information about the wallet.
+     */
+    public async get(address: string): Promise<AddressProfileResponse> {
+        const res = await this.client._getReq(`/api/v2/address/${address}`);
+        return (await res.json()) as AddressProfileResponse;
     }
 }
