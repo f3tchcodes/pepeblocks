feed6c418c7353d5e994846f59282b5ddff9b990
f3tch <f3tch.169@gmail.com>
2026-06-11 08:43:20 +0500

    JSDoc for get transaction method

--- src/modules/transactions.ts
+++ src/modules/transactions.ts
@@ -8,6 +8,11 @@ export class PepeTransactions {
         this.client = client;
     }
 
+    /**
+     * Fetches and decodes raw transaction details via transaction ID.
+     * @param txid The transaction ID string.
+     * @returns Fully decoded transaction information in JSON.
+     */
     public async get(txid: string): Promise<RawTransactionResponse> {
         const res = await this.client._getReq(`/api/getrawtransaction?txid=${txid}`);
         const parsedJson = (await res.json()) as RawTransactionResponse;
