3446a54df15081da8d5189c854d9a0bb9d7f3bbd
f3tch <f3tch.169@gmail.com>
2026-06-11 11:55:16 +0500

    current money supply

--- src/modules/market.ts
+++ src/modules/market.ts
@@ -6,4 +6,12 @@ export class PepeMarket {
     constructor(client: PepeClient) {
         this.client = client;
     }
+
+    /**
+     * Fetches the current money supply from the API.
+     * @returns Money supply number.
+     */
+    public async moneysupply(): Promise<number> {
+        return await this.client._getReqNumber(`/ext/getmoneysupply`);
+    }
 }
